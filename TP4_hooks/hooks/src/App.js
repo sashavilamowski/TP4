567293e81fc45ad833556a986b01a6b8d66fafa1
Sasha <132786467+sashavilamowski@users.noreply.github.com>
2024-05-02 09:00:35 -0300

    Add files via upload

---
+++ TP4_hooks/hooks/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Nombre from './components/Nombre/Nombre';
+
+function App() {
+  return (
+    <div className="App">
+      <Nombre></Nombre>
+    </div>
+    
+  );
+}
+
+export default App;
